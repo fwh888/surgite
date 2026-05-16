c2289a089906fa870f82979bcfc970ea14e56067
nicoleman0 <nicholashadleycoleman@gmail.com>
2026-05-16 12:53:17 +0100

    chore: remove build artifacts from tracking

--- build/lib/standup/formatter.py
+++
@@ -1,7 +0,0 @@
-from standup.models import Commit
-
-def format_commit(commit: Commit) -> str:
-    return f"[{commit.date}] {commit.message} ({commit.author}) <{commit.hash[:7]}>"
-
-def format_log(commits: list[Commit]) -> str:
-    return "\n".join(format_commit(c) for c in commits)
--- build/lib/standup/models.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class Commit:
-    hash: str
-    date: str
-    author: str
-    message: str
-
